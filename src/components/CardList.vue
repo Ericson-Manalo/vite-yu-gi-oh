c457635bd98b3d253931e1979b20bebd38962f38
Ericson-Manalo <ericmana98@gmail.com>
2023-06-20 00:32:30 +0200

    trying to get the image into the vfor

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -2,6 +2,8 @@
     <div class="row">
         <SingleCard v-for="card in cardList"
             :name="card.name"
+            :type="card.archetype"
+            :image="card.card_images.image_url_small"
         />  
 
     </div>
@@ -17,7 +19,7 @@ export default {
     name : 'CardList',
     data(){
         return{
-            cardList : [],
+            cardList : []
         }
     },
     components:{
@@ -27,8 +29,8 @@ export default {
     created(){
         axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
         .then((response) => {
-            console.log(response.data.data);
             this.cardList = response.data.data;
+            console.log(response.data.data.card_images)
         })
     }
 }
