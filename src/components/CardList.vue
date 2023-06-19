ef4f3c04504c7512ca1b38de5655ccb5881116a1
Ericson-Manalo <ericmana98@gmail.com>
2023-06-19 16:59:40 +0200

    created new component called CardList that refers to the list of card

---
+++ src/components/CardList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h4>
+            CardList
+        </h4>
+        <SingleCard/>
+
+    </div>
+</template>
+
+<script>
+import SingleCard from './SingleCard.vue';
+
+export default {
+    name : 'CardList',
+    data(){
+        return{
+
+        }
+    },
+    components:{
+        SingleCard
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
