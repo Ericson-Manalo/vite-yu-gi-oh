26bff13f1514c4e378809ef57fd126db4fb9adf5
Ericson-Manalo <ericmana98@gmail.com>
2023-06-19 17:00:52 +0200

    imported to my mainApp the component cardList

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,18 @@
 <template>
-    <h2>
-        Main
-    </h2>
+    <main>
+        <h2>
+            Main
+        </h2>
+        <h3>
+            <CardList/>
+        </h3>
+    </main>
+
 </template>
 
 <script>
+import CardList from './CardList.vue';
+
 export default {
     name: 'AppMain',
     data(){
@@ -13,7 +21,7 @@ export default {
         }
     },
     components:{
-        
+        CardList
     }
 }
 </script>
