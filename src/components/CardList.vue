57b527399ad3dce339878ea080388c7aca116b53
Ericson-Manalo <ericmana98@gmail.com>
2023-06-19 22:15:24 +0200

    imported the axios with link on it, and called what i needed on created

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,25 +1,35 @@
 <template>
-    <div>
-        <h4>
-            CardList
-        </h4>
-        <SingleCard/>
+    <div class="row">
+        <SingleCard v-for="card in cardList"
+            :name="card.name"
+        />  
 
     </div>
+
 </template>
 
 <script>
 import SingleCard from './SingleCard.vue';
+import axios from 'axios';
+
 
 export default {
     name : 'CardList',
     data(){
         return{
-
+            cardList : [],
         }
     },
     components:{
         SingleCard
+    },
+
+    created(){
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+        .then((response) => {
+            console.log(response.data.data);
+            this.cardList = response.data.data;
+        })
     }
 }
 </script>
