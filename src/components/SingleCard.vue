f0266678007e859ef756dc1919421726dd6ddbde
Ericson-Manalo <ericmana98@gmail.com>
2023-06-19 17:00:26 +0200

    created new component called single card that refers the creation of single card

---
+++ src/components/SingleCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h5>
+            Single Card
+        </h5>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'SingleCard',
+    data(){
+        return{
+
+        }
+    }
+    
+}
+</script>
+
+<style lang="">
+    
+</style>
